788417de5e7a03efdb84bad72a1b34952e55200c
Nathália Elen <90493304+NathaliaElen@users.noreply.github.com>
2025-01-04 11:44:04 -0300

    Primeiros passos

---
+++ hello-typescript/main.ts
@@ -0,0 +1,27 @@
+var meuNome = 'Nathália Elen';
+console.log(meuNome);
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+//ES6 ou ES 2015
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor) {
+    return valor * 2;
+});
+numeros.map(valor => valor * 2); //ES 2015
+
+class Matematica  {
+    soma(x, y) {
+        return x + y
+    };
+}
+
+var n1 = 'sssddff';
+n1 = 4;
+
+
